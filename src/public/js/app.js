3be877a7ffa643c822eb09f984efce624cd9e9da
leesoyuun <leesoyun4206@naver.com>
2024-04-20 23:06:09 +0900

    1.7 Nicknames part1

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,6 +1,12 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 function handleOpen() {
   console.log("Connected to Server");
@@ -9,22 +15,30 @@ function handleOpen() {
 socket.addEventListener("open", handleOpen); //소켓오픈했을때
 
 socket.addEventListener("message", (message) => {
-  console.log("New message:", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 }); //메시지를 받을때마다
 
 socket.addEventListener("close", () => {
   console.log("DisConnected from server");
 }); // 닫혔을때
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
+// setTimeout(() => {
+//   socket.send("hello from the browser!");
+// }, 10000);
 
 function handleSubmit(e) {
   e.preventDefault();
-  const input = messageForm.querySelector("input");
-  socket.send(input.value); //fe에서 be로 데이터 전송
-  input.value = ""; //이후 초기화
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("new_message", input.value));
+  input.value = "";
 }
 
+function handleNickSubmit(e) {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
