b7abebb9b2db3b76b9c68c111d6ecf7e23032748
leesoyuun <leesoyun4206@naver.com>
2024-04-20 22:17:07 +0900

    WebSocket Messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("connected to Server");
+}); //소켓오픈했을때
+
+socket.addEventListener("message", (message) => {
+  console.log("New message:", message.data);
+}); //메시지를 받을때마다
+
+socket.addEventListener("close", () => {
+  console.log("DisConnected from server");
+}); // 닫혔을때
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -15,4 +15,12 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+wss.on("connection", (socket) => {
+  console.log("Connected to Brwoser");
+  socket.on("close", () => console.log("Disconnected from the Browser"));
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.send("hello!!!"); //여기서 데이터를 보내면된다.
+});
 server.listen(3000, handleListen);
