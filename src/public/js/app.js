4604d719449e35324cb6d181bcfc78d08813627d
leesoyuun <leesoyun4206@naver.com>
2024-04-21 22:21:49 +0900

    3.2 Camera Switch

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -13,24 +13,39 @@ async function getCameras() {
   try {
     const devices = await navigator.mediaDevices.enumerateDevices();
     const cameras = devices.filter((device) => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
     cameras.forEach((camera) => {
       const option = document.createElement("option");
       option.value = camera.deviceId;
       option.innerText = camera.label;
+      if (currentCamera.label === camera.label) {
+        //선택된 카메라가 같을때 우리가 사용하고있는 카메라를 알 수 있다.
+        option.selected = true;
+      }
       camerasSelect.appendChild(option);
     });
   } catch (e) {
     console.log(e);
   }
 }
-async function getMedia() {
+async function getMedia(deviceId) {
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: "user" },
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: { deviceId: { exact: deviceId } },
+  };
   try {
     // 유저의 유저미디어 string을 줄것이다.
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true,
-    });
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId ? cameraConstraints : initialConstrains
+    );
     myFace.srcObject = myStream;
+    if (!deviceId) {
+      await getCameras();
+    }
     await getCameras();
   } catch (e) {
     console.log(e);
@@ -65,5 +80,9 @@ function handleCameraClick() {
   }
 }
 
+async function handleCameraChange() {
+  await getMedia(camerasSelect.value);
+}
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
