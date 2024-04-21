fa9ee5ef8d3ae1d49a1436847698c40ffa95e1ba
leesoyuun <leesoyun4206@naver.com>
2024-04-21 17:12:01 +0900

    2.8 Nickname

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -18,7 +18,7 @@ function addMessage(message) {
 
 function handleMessageSubmit(e) {
   e.preventDefault();
-  const input = room.querySelector("input");
+  const input = room.querySelector("#msg input");
   const value = input.value;
   socket.emit("new_message", input.value, roomName, () => {
     addMessage(`You: ${value}`);
@@ -26,13 +26,22 @@ function handleMessageSubmit(e) {
   input.value = "";
 }
 
+function handleNicknameSubmit(e) {
+  e.preventDefault();
+  const input = room.querySelector("#name input");
+  const value = input.value;
+  socket.emit("nickname", input.value);
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
-  const form = room.querySelector("form");
-  form.addEventListener("submit", handleMessageSubmit);
+  const msgFrom = room.querySelector("#msg");
+  const nameFrom = room.querySelector("#name");
+  msgFrom.addEventListener("submit", handleMessageSubmit);
+  nameFrom.addEventListener("submit", handleNicknameSubmit);
 }
 // backend에서 실행시킨것.
 // 사용하기 위해서는 가장 마지막 인자로 넘겨주면된다.
@@ -48,12 +57,12 @@ function handleRoomSubmit(e) {
 }
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => {
-  addMessage("someone Joined");
+socket.on("welcome", (user) => {
+  addMessage(`${user} arrived`);
 });
 
-socket.on("bye", () => {
-  addMessage("someone Left ㅠㅠ");
+socket.on("bye", (left) => {
+  addMessage(`${left} Left ㅠㅠ`);
 });
 
 socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -16,21 +16,25 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
+  socket["nickname"] = "Anon";
   socket.onAny((e) => {
     console.log(`Socket Event : ${e}`);
   });
   socket.on("enter_room", (roomName, done) => {
     socket.join(roomName); // 방에 들어가기 위함
     done();
-    socket.to(roomName).emit("welcome");
+    socket.to(roomName).emit("welcome", socket.nickname);
   });
   socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit("bye", socket.nickname)
+    );
   });
   socket.on("new_message", (msg, room, done) => {
-    socket.to(room).emit("new_message", msg);
+    socket.to(room).emit("new_message", `${socket.nickname} : ${msg}`);
     done();
   });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 }); //백엔드에서 connection을 받을 준비가 되었음.(fe - be connect)
 
 // const sockets = [];
